1ecf7bbe5d0aca8235bd384d6a1db6c7a66b5487
Isferix <ishefglatzel@gmail.com>
2026-05-10 01:06:49 -0300

    feat(api): new dependency added to support Serializing

--- api/src/endpoints/dependencies.py
+++ api/src/endpoints/dependencies.py
@@ -1,9 +1,15 @@
+from pathlib import Path
 from typing import Annotated
 
-from core.pydantic.application import PersonasHandler
+from core.pydantic.application import (
+    PersonasHandler,
+    PersonasSerializer,
+    SerializerHandler,
+)
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
+from ..adapters.excel import ExcelSerializer
 from ..adapters.repos.sqlalchemy import (
     PersonasRepository,
     SqlAlchemyPersonasRepository,
@@ -16,4 +22,11 @@ def get_personas_handler(db: Session = Depends(get_db)) -> PersonasHandler:
     return PersonasHandler(personas_repo)
 
 
+def get_serializer_handler(db: Session = Depends(get_db)) -> SerializerHandler:
+    personas_serializer: PersonasSerializer = ExcelSerializer(Path("/storage/excel/"))
+    personas_repo: PersonasRepository = SqlAlchemyPersonasRepository(db)
+    return SerializerHandler(personas_serializer, personas_repo)
+
+
 personas_dependency = Annotated[PersonasHandler, Depends(get_personas_handler)]
+serializer_dependency = Annotated[SerializerHandler, Depends(get_serializer_handler)]
