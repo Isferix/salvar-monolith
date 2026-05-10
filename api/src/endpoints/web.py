aa9cc37954b4d10fc1d5de41b461647069119e9c
Isferix <ishefglatzel@gmail.com>
2026-05-10 00:19:58 -0300

    feat(api): Dependency Injection pattern applied to expose and use PersonasHandler in endpoints

--- api/src/endpoints/web.py
+++ api/src/endpoints/web.py
@@ -1,7 +1,24 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from typing import Annotated
 
+from core.pydantic.application import PersonasHandler
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+
+from ..adapters.repos.sqlalchemy import (
+    PersonasRepository,
+    SqlAlchemyPersonasRepository,
+)
+from ..infrastructure.db.engine import get_db
 from ..utils.responses import render, renderBase
+
+
+def get_personas_handler(db: Session = Depends(get_db)) -> PersonasHandler:
+    personas_repo: PersonasRepository = SqlAlchemyPersonasRepository(db)
+    return PersonasHandler(personas_repo)
+
+
+personas_dependency = Annotated[PersonasHandler, Depends(get_personas_handler)]
 
 web = APIRouter()
 
